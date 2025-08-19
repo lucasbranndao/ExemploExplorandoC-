e41ea91a5fba1ed73954c5951bbaeeeff482ef90
Lucas Brandão <lucasbranndao@gmail.com>
2025-08-19 14:04:26 -0300

    Deserializando

--- Models/Venda.cs
+++ Models/Venda.cs
@@ -1,20 +1,12 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection.Emit;
 using System.Threading.Tasks;
 
 namespace ExemploExplorando.Models
 {
     public class Venda
     {
-        public Venda(int id, string produto, decimal preco, DateTime dataVenda)
-        {
-            Id = id;
-            Produto = produto;
-            Preco = preco;
-            DataVenda = dataVenda;
-        }
         public int Id { get; set; }
         public string Produto { get; set; }
         public decimal Preco { get; set; }
--- Program.cs
+++ Program.cs
@@ -5,21 +5,54 @@ using System.Transactions;
 using System.Data;
 using Newtonsoft.Json;
 
-DateTime dataAtual = DateTime.Now;
 
-List<Venda> listaVendas = new List<Venda>();
+string conteudoArquivo = File.ReadAllText("Arquivos/vendas.json");
 
-Venda v1 = new Venda(1, "Material de escritorio", 23.00M, dataAtual);
-Venda v2 = new Venda(1, "Licença de software", 110.00M, dataAtual);
+List<Venda> listaVenda = JsonConvert.DeserializeObject<List<Venda>>(conteudoArquivo);
 
-listaVendas.Add(v1);
-listaVendas.Add(v2);
+foreach (Venda venda in listaVenda)
+{
+    Console.WriteLine($" Id: {venda.Id}, Produto {venda.Produto}, Preço: {venda.Preco}, Data: {venda.DataVenda.ToString("dd/MM/yyyy HH:mm")}");
+}
 
-string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
 
-File.WriteAllText("Arquivos/vendas.json", serializado);
 
-Console.WriteLine(serializado);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// DateTime dataAtual = DateTime.Now;
+
+// List<Venda> listaVendas = new List<Venda>();
+
+// Venda v1 = new Venda(1, "Material de escritorio", 23.00M, dataAtual);
+// Venda v2 = new Venda(1, "Licença de software", 110.00M, dataAtual);
+
+// listaVendas.Add(v1);
+// listaVendas.Add(v2);
+
+// string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
+
+// File.WriteAllText("Arquivos/vendas.json", serializado);
+
+// Console.WriteLine(serializado);
 
 
 
