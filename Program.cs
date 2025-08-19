ac286bfa2f33914161989d54e27c7268e6bcfc2f
Lucas Brandão <lucasbranndao@gmail.com>
2025-08-19 13:26:23 -0300

    serializando colecao

--- Program.cs
+++ Program.cs
@@ -5,10 +5,15 @@ using System.Transactions;
 using System.Data;
 using Newtonsoft.Json;
 
+List<Venda> listaVendas = new List<Venda>();
 
 Venda v1 = new Venda(1, "Material de escritorio", 23.00M);
+Venda v2 = new Venda(1, "Licença de software", 110.00M);
 
-string serializado = JsonConvert.SerializeObject(v1, Formatting.Indented);
+listaVendas.Add(v1);
+listaVendas.Add(v2);
+
+string serializado = JsonConvert.SerializeObject(listaVendas, Formatting.Indented);
 
 File.WriteAllText("Arquivos/vendas.json", serializado);
 
